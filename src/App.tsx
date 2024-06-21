029a1a2b2fa140cf516c10a22bd4807ebd34d34d
dy7791 <dy7791@naver.com>
2024-06-21 11:49:47 +0900

    Base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Suspense } from "react";
+import { ErrorBoundary, Loading } from "./components";
 import "./App.css";
 
+const A = () => {
+  throw new Error();
+};
+
 function App() {
-  return <></>;
+  return (
+    <ErrorBoundary>
+      <Suspense fallback={<Loading />}>
+        <Loading />
+      </Suspense>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
