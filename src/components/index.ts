df01d08124ac923ca6d048ff12d70dc9dd0aef8a
dy7791 <dy7791@naver.com>
2024-06-24 14:41:40 +0900

    compoent index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,22 @@
 // Common
 import ErrorBoundary from "./Common/ErrorBoundary";
 import Loading from "./Common/Loading";
+import RenderProps from "./Common/RenderProps";
 
 // CommonLayout
 import Header from "./CommonLayout/Header";
 import Footer from "./CommonLayout/Footer";
 
-export { ErrorBoundary, Loading, Header, Footer };
+// Products
+import ProductFetch from "./Products/ProductFetch";
+import ProductItem from "./Products/ProductItem";
+
+export {
+  ErrorBoundary,
+  Loading,
+  RenderProps,
+  Header,
+  Footer,
+  ProductFetch,
+  ProductItem,
+};
